d3e94fcb45e7b6dc4fdd8dc37640eae4d7bfe6fc
Tai Luis <tailuis7@gmail.com>
2017-02-04 16:27:08 +0700

    Done part 11 in tutorial.

--- js/controllers/quiz.js
+++ js/controllers/quiz.js
@@ -9,5 +9,11 @@
 			var vm = this;
 			vm.quizMetrics = quizMetrics;
 			vm.dataService = DataService;
+			vm.questionAnswered = questionAnswered;
+			vm.activeQuestion = 0;
+
+			function questionAnswered() {
+				
+			}
 		}
 })();
