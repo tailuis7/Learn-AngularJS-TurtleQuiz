a16a685981ebc46aa67b1cbbafe53cb2ee316dcf
Tai Luis <tailuis7@gmail.com>
2017-02-04 17:29:42 +0700

    Done part 14 in tutorial ( For ng-repeat).

--- js/controllers/quiz.js
+++ js/controllers/quiz.js
@@ -1,43 +1,48 @@
 (function() {
-	angular
-		.module("turtleFacts")
-		.controller("quizCtrl", QuizController);
+    angular
+        .module("turtleFacts")
+        .controller("quizCtrl", QuizController);
 
-		QuizController.$inject = ['quizMetrics', 'DataService']; //Another way to inject dependencies
+    QuizController.$inject = ['quizMetrics', 'DataService']; //Another way to inject dependencies
 
-		function QuizController(quizMetrics, DataService) {
-			var vm = this;
-			vm.quizMetrics = quizMetrics;
-			vm.dataService = DataService;
-			vm.questionAnswered = questionAnswered;
-			vm.setActiveQuestion = setActiveQuestion;
-			vm.activeQuestion = 0;
+    function QuizController(quizMetrics, DataService) {
+        var vm = this;
+        vm.quizMetrics = quizMetrics;
+        vm.dataService = DataService;
+        vm.questionAnswered = questionAnswered;
+        vm.setActiveQuestion = setActiveQuestion;
+        vm.activeQuestion = 0;
+        vm.selectAnswer = selectAnswer;
 
-			var numQuestionsAnswered = 0;
+        var numQuestionsAnswered = 0;
 
-			function setActiveQuestion() {
-				// vm.activeQuestion++;
-				var breakOut = false;
-				var quizLength = DataService.quizQuestions.length - 1;
+        function setActiveQuestion() {
+            // vm.activeQuestion++;
+            var breakOut = false;
+            var quizLength = DataService.quizQuestions.length - 1;
 
-				while (!breakOut) {
-					vm.activeQuestion = vm.activeQuestion < quizLength ? ++vm.activeQuestion : 0;
-					if (DataService.quizQuestions[vm.activeQuestion].selected === null) {
-						breakOut = true;
-					}
-				}
-			}
+            while (!breakOut) {
+                vm.activeQuestion = vm.activeQuestion < quizLength ? ++vm.activeQuestion : 0;
+                if (DataService.quizQuestions[vm.activeQuestion].selected === null) {
+                    breakOut = true;
+                }
+            }
+        }
 
-			function questionAnswered() {
-				var quizLength = DataService.quizQuestions.length;
+        function questionAnswered() {
+            var quizLength = DataService.quizQuestions.length;
 
-				if (DataService.quizQuestions[vm.activeQuestion].selected !== null) {
-					numQuestionsAnswered++;
-					if (numQuestionsAnswered >= quizLength) {
-						//Finalize quiz
-					}
-				}
-				vm.setActiveQuestion();
-			}
-		}
+            if (DataService.quizQuestions[vm.activeQuestion].selected !== null) {
+                numQuestionsAnswered++;
+                if (numQuestionsAnswered >= quizLength) {
+                    //Finalize quiz
+                }
+            }
+            vm.setActiveQuestion();
+        }
+
+        function selectAnswer(index) {
+            DataService.quizQuestions[vm.activeQuestion].selected = index;
+        }
+    }
 })();
