960f49d5510c16de6e01d81ba68da4e6e658f3e4
Tai Luis <tailuis7@gmail.com>
2017-02-04 17:01:17 +0700

    Done part 12 in tutorial.

--- js/controllers/quiz.js
+++ js/controllers/quiz.js
@@ -10,10 +10,34 @@
 			vm.quizMetrics = quizMetrics;
 			vm.dataService = DataService;
 			vm.questionAnswered = questionAnswered;
+			vm.setActiveQuestion = setActiveQuestion;
 			vm.activeQuestion = 0;
 
+			var numQuestionsAnswered = 0;
+
+			function setActiveQuestion() {
+				// vm.activeQuestion++;
+				var breakOut = false;
+				var quizLength = DataService.quizQuestions.length - 1;
+
+				while (!breakOut) {
+					vm.activeQuestion = vm.activeQuestion < quizLength ? ++vm.activeQuestion : 0;
+					if (DataService.quizQuestions[vm.activeQuestion].selected === null) {
+						breakOut = true;
+					}
+				}
+			}
+
 			function questionAnswered() {
-				
+				var quizLength = DataService.quizQuestions.length;
+
+				if (DataService.quizQuestions[vm.activeQuestion].selected !== null) {
+					numQuestionsAnswered++;
+					if (numQuestionsAnswered >= quizLength) {
+						//Finalize quiz
+					}
+				}
+				vm.setActiveQuestion();
 			}
 		}
 })();
